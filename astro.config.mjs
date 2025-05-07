97a8d646d4f780a003ebdf9dabdde89936864c9d
Thomas Bannister <thomtzen@gmail.com>
2025-05-07 03:12:14 +0200

    passthroughImageService()

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 // import animejs from 'animejs';
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   vite: {
     plugins: [tailwindcss()],
   },
